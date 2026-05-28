a064fcf3b8bd2c83293d9dcc020e33586b5b376f
dan7el11 <md.danieldonoso@gmail.com>
2026-05-28 16:03:03 -0500

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,8 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
-// Estas variables deben coincidir con tus credenciales de Firebase Console
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCwp8KhisgZ64tdYHAq27k-_3O76MFNijQ",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "polla-austrogas.firebaseapp.com",
@@ -12,9 +12,8 @@ const firebaseConfig = {
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:352531542943:web:0b5d45ded7fc46bfac43aa"
 };
 
-// Inicialización de la aplicación de Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exportar los servicios para usarlos en toda la web app
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
