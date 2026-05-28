f50c06d6a3b3f5bf1c1e6cf38ac61a1c7319e822
dan7el11 <md.danieldonoso@gmail.com>
2026-05-28 13:33:01 -0500

    Update DetalleTrabajador.tsx

--- src/pages/DetalleTrabajador.tsx
+++ src/pages/DetalleTrabajador.tsx
@@ -434,7 +434,7 @@ export default function DetalleTrabajador() {
           }
         }
       }
-       y = (pdf as any).lastAutoTable.finalY;
+      y = (pdf as any).lastAutoTable.finalY;
 
     if (ev.examenFisicoHallazgos && ev.examenFisicoHallazgos.length > 0) {
       const lineasFisico = ev.examenFisicoHallazgos
