a4d7d37e76bef885e489fab890dc71931208e71e
dan7el11 <md.danieldonoso@gmail.com>
2026-05-29 10:07:51 -0500

    Update firebase.ts

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -7,7 +7,7 @@ const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY || "AIzaSyCwp8KhisgZ64tdYHAq27k-_3O76MFNijQ",
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN || "polla-austrogas.firebaseapp.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID || "polla-austrogas",
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "polla-austrogas.appspot.com",
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET || "polla-austrogas.firebasestorage.app",
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID || "352531542943",
   appId: import.meta.env.VITE_FIREBASE_APP_ID || "1:352531542943:web:0b5d45ded7fc46bfac43aa"
 };
